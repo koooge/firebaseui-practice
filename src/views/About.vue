d40b7c9142d3eb653e2c2c11a94c76b7a40f348b
koooge <koooooge@gmail.com>
2020-11-16 23:42:29 +0100

    feat: Show currentUser
    Signed-off-by: koooge <koooooge@gmail.com>

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <pre id="profile"><code>{{ profile }}</code></pre>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "vue";
+import firebase from "firebase/app";
+
+export default defineComponent({
+  name: "About",
+  setup() {
+    const profile = JSON.stringify(firebase.auth().currentUser, null, 4);
+    return { profile };
+  }
+});
+</script>
+
+<style scoped>
+#profile {
+  text-align: left;
+}
+</style>
